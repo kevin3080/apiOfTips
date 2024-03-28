e04243ceb239274fce629a512c072a0e103ff448
Kevin <kevinpernia29@gmail.com>
2024-03-27 20:56:26 -0500

    update/add swagger documentation

--- app/Http/Controllers/ChineseTipController.php
+++ app/Http/Controllers/ChineseTipController.php
@@ -5,6 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\ChineseTip;
 use Illuminate\Http\JsonResponse;
 
+/**
+     * @OA\Get(
+     *   path="/tips/chinese-tips",
+     *   operationId="/tips/chinese-tips",
+     *   tags={"tips"},
+     *   summary="Retrieve tips in Chinese",
+     *   description="Returns a list of all tips, regardless of their language or origin.",
+     *   @OA\Response(
+     *     response="200",
+     *     description="A collection of tips",
+     *   
+     *   ),
+     *   @OA\Response(
+     *     response="404",
+     *     description="Not Found"
+     *   )
+     * )
+    */
+
 class ChineseTipController extends Controller
 {
 
